8da36f66fbc86169b25e74cd510f799615d1f046
Gabriel Amboss Pinto <gabriel.amboss@gmail.com>
2013-05-18 23:46:24 -0300

    testing

---
+++ Collision/Collision/Minimap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Collision
+{
+    class Minimap
+    {
+        MapManager mapManager;
+
+        public Minimap(MapManager mapManager)
+        {
+            this.mapManager = mapManager;
+        }
+
+
+    }
+}
